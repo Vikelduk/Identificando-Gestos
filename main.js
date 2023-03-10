aaf24f00db11ba1007ec5df2883ebe070ebb1978
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-03-10 15:06:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,103 +1,38 @@
-prediction = "";
+var noseX = 0;
+var noseY = 0;
 
-Webcam.set(
-    { 
-        width:350, 
-        height:300, 
-        imageFormat : 'png', 
-        pngQuality:90 
-    }); 
-    
-camera = document.getElementById("camera"); 
-Webcam.attach('#camera');
+var difference = 0;
 
+var rightWristX = 0;
+var leftWristX = 0;
 
-function takesnapshot()
+function setup()
 {
-    Webcam.snap(function(data_uri)
-    {
-        document.getElementById("resultado").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
-    });
-    fotoTirada();
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+    video.position(75, 50);
+
+    canvas = createCanvas(600, 450);
+    canvas.position(675, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function fotoTirada()
+function modelLoaded()
 {
-    console.log("Foto Tirada com sucesso!");
+    console.log("O poseNet foi Inicializado com Sucesso!");
 }
 
-console.log('ml5 version:', ml5.version);
-
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/A-gv2SyvG/model.json', modeloCarregado);
-
-function modeloCarregado()
+function gotPoses(results)
 {
-    console.log("Modelo carregado!")
-}
-
-function speak()
-{
-    var synth = window.speechSynthesis;
-    speakData1 = "A previsão é " + prediction;
-    var utterThis = new SpeechSynthesisUtterance(speakData1);
-    synth.speak(utterThis);
-
-}
-
-function check()
-{
-    img = document.getElementById('captured_image');
-    classifier.classify(img, gotResults);
-}
-
-function gotResults (error, results)
-{
-    if (error)
-    {
-        console.error(error)
-    }
-    else
+    if (results.lenght > 0)
     {
         console.log(results);
-
-        document.getElementById("resultadoEmocao").innerHTML = results[0].label;
-        prediction = results[0].label;
-        
-        speak();
-
-        if (results[0].label == "like")
-        {
-            document.getElementById("updateEmoji").innerHTML = "&#128077;";
-        }
-
-        if (results[0].label == "dislike")
-        {
-            document.getElementById("updateEmoji").innerHTML = "&#128078;";
-        }
-
-        if (results[0].label == "ok")
-        {
-            document.getElementById("updateEmoji").innerHTML = "&#128076;";
-        }
-
-        if (results[0].label == "paz")
-        {
-            document.getElementById("updateEmoji").innerHTML = "&#9996;&#65039;";
-        }
-
-        if (results[0].label == "soco")
-        {
-            document.getElementById("updateEmoji").innerHTML = "&#9994;";
-        }
-
-        if (results[0].label == "ola")
-        {
-            document.getElementById("updateEmoji").innerHTML = "&#128075;";
-        }
     }
 }
 
-
-
-    
-        
+function draw()
+{
+    background('#87CEEB');   
+}
